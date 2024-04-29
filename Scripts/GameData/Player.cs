089a7dcbd0a1b6ae14651b762292644d4910d2f3
seungdo1234 <86179438+seungdo1234@users.noreply.github.com>
2024-04-29 20:37:55 +0900

    :art: Unit Class 추가

--- Scripts/GameData/Player.cs
+++ Scripts/GameData/Player.cs
@@ -3,19 +3,13 @@ using Newtonsoft.Json.Linq;
 
 namespace TextRPG
 {
-    public class Player
+    public class Player:Unit
     {
     
         // 플레이어 경험치
         private int[] levelExp = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }; // 레벨 별 경험치 통
 
         public PlayerClass PlayerClass { get;  set; }
-        [JsonProperty] public string Name { get; private set; }
-        [JsonProperty] public int Level { get; private set; }
-        [JsonProperty] public float Atk { get; private set; }
-        [JsonProperty]  public float Def { get; private set; }
-        [JsonProperty]  public int Health { get; private set; }
-        [JsonProperty] public int MaxHealth { get; private set; }
         public int Gold { get; set; }
         [JsonProperty] public int Exp { get; private set; }
         public Item EquipAtkItem { get; set; }
@@ -30,6 +24,9 @@ namespace TextRPG
             MaxHealth = 100;
             Health = MaxHealth;
             Gold = 10000;
+            AvoidChance = 10;
+            CriticalChance = 16;
+            CriticalDamage = 1.6f;
         }
 
         public string GetPlayerClass(PlayerClass _playerClass) // 플레이어의 직업 별 이름 반환 
@@ -99,5 +96,7 @@ namespace TextRPG
             Atk += 0.5f;
             Def += 1;
         }
+
+
     }
 }
---
+++ Scripts/GameData/Unit.cs
@@ -0,0 +1,37 @@
+﻿
+using Newtonsoft.Json;
+using System;
+
+namespace TextRPG
+{
+    public class Unit
+    {
+
+        [JsonProperty] public string Name { get; protected set; }
+        [JsonProperty] public int Level { get; protected set; }
+        [JsonProperty] public float Atk { get; protected set; }
+        [JsonProperty] public float Def { get; protected set; }
+        [JsonProperty] public int Health { get; protected set; }
+        [JsonProperty] public int MaxHealth { get; protected set; }
+        [JsonProperty] public int AvoidChance { get; protected set; }
+        [JsonProperty] public int CriticalChance { get; protected set; }
+        [JsonProperty] public float CriticalDamage { get; protected set; }
+
+
+        private Random random = new Random();
+        public void OnDamaged(int health) // 피격
+        {
+            this.Health -= health;
+        }
+
+        virtual public void Attack(Unit unit)
+        {
+            int per = random.Next(1, 101);
+
+            if (per <= CriticalChance)
+            {
+
+            }
+        }
+    }
+}
