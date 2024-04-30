4c4f9c82a877e7a7e0b2e66e3a325cb93bded860
Chloe <144107013+Charen523@users.noreply.github.com>
2024-04-30 16:44:48 +0900

    [feat] EnemySkill Class Made

---
+++ Scripts/GameData/EnemySkill.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPG.Scripts.GameData
+{
+    public class EnemySkill
+    {
+        private string Name { get; }
+        private string Explain { get; }
+        private int Damage { get; }
+
+    }
+}
--- Scripts/Manager/EnemyDataManager.cs
+++ Scripts/Manager/EnemyDataManager.cs
@@ -14,35 +14,25 @@ namespace TextRPG
         private List<Enemy> MonsterDB;
         //스테이지별 몬스터 리스트
         private List<Enemy> SpawnMonsters;
+        private Enemy BossMonster;
 
         public void Init()
         {
             MonsterDB = new List<Enemy>();
             SpawnMonsters = new List<Enemy>();
 
-            MonsterDB.Add(new Enemy("사나운 토끼", 1, 5, 0, 20, 
-                1, 1, 1));
-            MonsterDB.Add(new Enemy("노을의 늑대개", 2, 10, 3, 30,
-                1, 1, 1));
-            MonsterDB.Add(new Enemy("피투성이 망토", 3, 15, 6, 40,
-                1, 1, 1));
-            MonsterDB.Add(new Enemy("흐느끼는 유령", 4, 20, 9, 50,
-                1, 1, 1));
-            MonsterDB.Add(new Enemy("늘어나는 그림자", 5, 25, 12, 60,
-                1, 1, 1));
-            MonsterDB.Add(new Enemy("노래하는 물고기", 6, 30, 15, 70,
-                1, 1, 1));
-            MonsterDB.Add(new Enemy("외로운 불귀신", 7, 35, 18, 80,
-                1, 1, 1));
-            MonsterDB.Add(new Enemy("독수리사자", 8, 40, 21, 90,
-                1, 1, 1));
-            MonsterDB.Add(new Enemy("흰머리 호랑이", 9, 45, 24, 100,
-                1, 1, 1));
-            MonsterDB.Add(new Enemy("녹안의 악마", 10, 50, 27, 120,
-                1, 1, 1));
+            MonsterDB.Add(new Enemy("사나운 토끼", 1, 5, 0, 20));
+            MonsterDB.Add(new Enemy("노을의 늑대개", 2, 10, 3, 30));
+            MonsterDB.Add(new Enemy("강을 건넌 사람", 3, 15, 6, 40));
+            MonsterDB.Add(new Enemy("흐느끼는 유령", 4, 20, 9, 50));
+            MonsterDB.Add(new Enemy("늘어나는 그림자", 5, 25, 12, 60));
+            MonsterDB.Add(new Enemy("노래하는 물고기", 6, 30, 15, 70));
+            MonsterDB.Add(new Enemy("외로운 불귀신", 7, 35, 18, 80));
+            MonsterDB.Add(new Enemy("독수리사자", 8, 40, 21, 90));
+            MonsterDB.Add(new Enemy("흰머리 호랑이", 9, 45, 24, 100));
+            MonsterDB.Add(new Enemy("녹안의 악마", 10, 50, 27, 120));
 
-            //MonsterDB.Add(new Enemy("휘몰아치는 강철이", 20, 100, 50, 1000,
-            //    1, 1, 1)); 보스라서 일단 주석처리.
+            BossMonster = (new Enemy("휘몰아치는 강철이", 0, 100, 50, 1000));  //보스lv은 특수처리.
         }
 
         
@@ -87,6 +77,11 @@ namespace TextRPG
 
             selectedCombination = levelCombinations[random.Next(levelCombinations.Count)];
             return selectedCombination;
+        }
+
+        public Enemy GetBoss()
+        {
+            return BossMonster;
         }
     }
 }
