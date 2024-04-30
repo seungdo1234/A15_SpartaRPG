c6f831566130f7241658524119f39aa480d70eb8
Chloe <144107013+Charen523@users.noreply.github.com>
2024-04-30 10:03:11 +0900

    [feat] Enemy&EnemyDataManger
    Enemy의 정보를 등록할 생성자 추가
    
    EnemyDatamanager 생성 및 일부 몬스터 데이터 입력

--- Scripts/GameData/Enemy.cs
+++ Scripts/GameData/Enemy.cs
@@ -19,5 +19,32 @@ namespace TextRPG
             CriticalDamage = 1.6f;
         }
 
+        public Enemy(string Name, int Level, float Atk, float Def,
+            int MaxHealth, int AvoidChance, int CriticalChance, float CriticalDamage)
+        {
+            this.Name = Name;
+            this.Level = Level;
+            this.Atk = Atk;
+            this.Def = Def;
+            this.MaxHealth = MaxHealth; //몬스터 회복 기믹...?
+            this.Health = MaxHealth;
+            this.AvoidChance = AvoidChance;
+            this.CriticalChance = CriticalChance;
+            this.CriticalDamage = CriticalDamage;
+        }
+
+        public override string ToString()
+        {
+            if (Level > 9)
+            {
+                return $"Lv.{Level} {Name}\tHP {Health}/{MaxHealth}";
+            }
+            else
+            {
+                return $"Lv.{Level}  {Name}\tHP {Health}/{MaxHealth}";
+            }
+
+            
+        }
     }
 }
---
+++ Scripts/Manager/EnemyDataManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPG
+{
+    public class EnemyDataManager
+    {
+        public static EnemyDataManager instance = new EnemyDataManager();
+
+        //전체 몬스터 리스트
+        private List<Enemy> MonsterDB;
+        //스테이지별 몬스터 리스트
+        private List<Enemy> SpawnMonsters;
+
+        public void Init()
+        {
+            MonsterDB = new List<Enemy>();
+            SpawnMonsters = new List<Enemy>();
+
+            MonsterDB.Add(new Enemy("중세 토끼", 1, 5, 0, 20, 
+                1, 1, 1));
+            MonsterDB.Add(new Enemy("노을의 늑대개", 2, 10, 3, 30,
+                1, 1, 1));
+            MonsterDB.Add(new Enemy("빨간 망토", 3, 15, 6, 40,
+                1, 1, 1));
+            MonsterDB.Add(new Enemy("흐느끼는 유령", 4, 20, 9, 50,
+                1, 1, 1));
+            MonsterDB.Add(new Enemy("작은 어둑시니", 5, 25, 12, 60,
+                1, 1, 1));
+            MonsterDB.Add(new Enemy("", 6, 30, 15, 70,
+                1, 1, 1));
+            MonsterDB.Add(new Enemy("ㅇ", 7, 35, 18, 80,
+                1, 1, 1));
+            MonsterDB.Add(new Enemy("", 8, 40, 21, 90,
+                1, 1, 1));
+            MonsterDB.Add(new Enemy("흰머리 호랑이", 9, 45, 24, 100,
+                1, 1, 1));
+            MonsterDB.Add(new Enemy("a", 10, 50, 27, 120,
+                1, 1, 1));
+
+            MonsterDB.Add(new Enemy("휘몰아치는 강철이", 20, 100, 50, 500,
+                1, 1, 1));
+        }
+    }
+}
