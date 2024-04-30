c0d45809b53b143b7590d788ef17c2c950e6f9d2
Chloe <144107013+Charen523@users.noreply.github.com>
2024-04-30 19:17:35 +0900

    [feat] enemySkill added

--- Scripts/GameData/EnemySkill.cs
+++ Scripts/GameData/EnemySkill.cs
@@ -9,13 +9,13 @@ namespace TextRPG
     public class EnemySkill //일반몹: 필살기. //boss: 스킬.
     {
         private string Name { get; }
-        private int Damage { get; }
-        private string Content { get; } //플레이어와 달리 정확한 정보x, 재미요소o.
+        private float Damage { get; }
+        private string Content { get; } //간단한 스킬설명
 
-        public EnemySkill(string name, int baseDamage, string content)
+        public EnemySkill(string name, int level, float baseDamage, string content)
         {
             Name = name;
-            Damage = baseDamage;
+            Damage = baseDamage * ((10 + level)/10);
             Content = content;
         }
 
