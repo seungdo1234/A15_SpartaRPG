29aa39346c04bab082aa6095e3bc63ddc50157ac
Chloe <144107013+Charen523@users.noreply.github.com>
2024-04-30 11:52:01 +0900

    [feat] Enemy Random Encount

--- Scripts/Manager/EnemyDataManager.cs
+++ Scripts/Manager/EnemyDataManager.cs
@@ -20,11 +20,11 @@ namespace TextRPG
             MonsterDB = new List<Enemy>();
             SpawnMonsters = new List<Enemy>();
 
-            MonsterDB.Add(new Enemy("중세 토끼", 1, 5, 0, 20, 
+            MonsterDB.Add(new Enemy("사나운 토끼", 1, 5, 0, 20, 
                 1, 1, 1));
             MonsterDB.Add(new Enemy("노을의 늑대개", 2, 10, 3, 30,
                 1, 1, 1));
-            MonsterDB.Add(new Enemy("빨간 망토", 3, 15, 6, 40,
+            MonsterDB.Add(new Enemy("피투성이 망토", 3, 15, 6, 40,
                 1, 1, 1));
             MonsterDB.Add(new Enemy("흐느끼는 유령", 4, 20, 9, 50,
                 1, 1, 1));
@@ -41,41 +41,46 @@ namespace TextRPG
             MonsterDB.Add(new Enemy("녹안의 악마", 10, 50, 27, 120,
                 1, 1, 1));
 
-            MonsterDB.Add(new Enemy("휘몰아치는 강철이", 20, 100, 50, 500,
-                1, 1, 1));
+            //MonsterDB.Add(new Enemy("휘몰아치는 강철이", 20, 100, 50, 1000,
+            //    1, 1, 1)); 보스라서 일단 주석처리.
         }
 
         
         public List<Enemy> GetSpawnMonsters(int CurrentDungeonLevel)
         {
-            int totalLevelLimit = CurrentDungeonLevel + 2;
-            int[] monsterLevels = randomMonsterEncount(totalLevelLimit);
+            int totalLevelLimit = CurrentDungeonLevel * 2;
+            int maxLevel = 10;
+            int[] monsterLevels = randomMonsterEncount(totalLevelLimit, maxLevel);
 
-            for (int i = 0; i < monsterLevels.Length; i++)
+            foreach (int i in monsterLevels)
             {
-                SpawnMonsters.Add(MonsterDB[i + 1]);
+                if (i == 0) continue;
+
+                SpawnMonsters.Add(MonsterDB[i - 1]);
             }
-
-
 
             return SpawnMonsters;
         }
 
-        private int[] randomMonsterEncount(int totalLevelLimit)
+        private int[] randomMonsterEncount(int totalLevelLimit, int maxLevel)
         {
             List<int[]> levelCombinations = new List<int[]>();
             int[] selectedCombination;
             Random random = new Random();
 
-            for (int i = 0; i <= totalLevelLimit; i++)
+            maxLevel = Math.Min(totalLevelLimit, maxLevel);
+
+            for (int i = 0; i <= maxLevel; i++)
             {
-                for (int j = 0; i + j <= totalLevelLimit; j++)
+                for (int j = 0; i + j <= maxLevel; j++)
                 {
-                    for (int k = 0; i + j + k <= totalLevelLimit; k++)
+                    for (int k = 0; i + j + k <= maxLevel; k++)
                     {
                         int l = totalLevelLimit - (i + j + k);
-                        if (l <= totalLevelLimit)
-                            levelCombinations.Add(new int[] { i, j, k, l});
+                        if (l >= 0 && l <= maxLevel)
+                        {
+                            levelCombinations.Add(new int[] { i, j, k, l });
+                        }
                     }
                 }
             }
