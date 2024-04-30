83708c4d6fc6928b0cd14d2b88629dcd028c8797
seungdo1234 <86179438+seungdo1234@users.noreply.github.com>
2024-04-30 10:25:02 +0900

    :adhesive_bandage:  주석 추가

--- Scripts/Manager/GameManager.cs
+++ Scripts/Manager/GameManager.cs
@@ -5,10 +5,10 @@ namespace TextRPG
     {
         public static GameManager instance = new GameManager();
 
+
+        // 4.30 J / 자동구현 프로퍼티로 변경 
         public Player Player { get;  set; }
-
         public DungeonManager Dungeon { get; private set; }
-
         public SaveSystem SaveSystem  { get; private set; }
 
         public void Init() // 객체 초기화
