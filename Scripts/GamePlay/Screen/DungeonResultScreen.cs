9b7bb0ecc772f78e4f5339b73af5ca07f697e388
Devwons93 <jion933@gmail.com>
2024-04-30 10:38:25 +0900

    Feat: 플레이어 직업 변경 메소드 추가 및 유닛 마나 프로퍼티 추가
    Unit 마나 관련 프로퍼티 추가
    ChangePlayerClass 메소드 추가
    직업별 보너스 추가 스탯 적용
    Attack() 메소드 치명타 계산식 변경
    CriticalDamage에 의한 데미지 배수 적용

--- Scripts/GamePlay/Screen/DungeonResultScreen.cs
+++ Scripts/GamePlay/Screen/DungeonResultScreen.cs
@@ -67,7 +67,7 @@ namespace TextRPG
 
                 if (per <= 40) // 실패
                 {
-                    gm.Player.OnDamaged(prevHealth / 2);
+                    gm.Player.IsDamaged(prevHealth / 2);
                     return false;
                 }
                 else // 클리어
@@ -81,7 +81,7 @@ namespace TextRPG
         private void DungeonClearReward() // 던전 클리어 보상 지급 함수
         {
             int reduce = rand.Next(20, 36) + (gm.Dungeon.GetRecommandedDef(dif) - (int)gm.Player.Def );
-            gm.Player.OnDamaged(reduce);
+            gm.Player.IsDamaged(reduce);
             
             prevGold = gm.Player.Gold; 
             gm.Player.Gold += gm.Dungeon.GetDungeonReward(dif) + (gm.Dungeon.GetDungeonReward(dif) * (int)(gm.Player.Atk * 100) / 10000);
