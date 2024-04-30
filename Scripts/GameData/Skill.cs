e994b79f0a151fa4a09d7782dca847399366c246
Devwons93 <jion933@gmail.com>
2024-04-30 15:52:01 +0900

    feat: Skill 구현
    스킬 정보를 저장하는 SkillData 클래스와 스킬목록을 저장하는 Skills 클래스, Skills를 상속받는 전사 스킬목록 WarriorSkills 구현
    데미지 계산식 변경 > 소수점 반올림 적용 및 치명타확인하는 bool타입 반환

---
+++ Scripts/GameData/Skill.cs
@@ -0,0 +1,12 @@
+﻿namespace TextRPG
+{
+    internal class Skill
+    {
+        public string Name { get; private set; }
+        public int TargetCount {  get; private set; }
+        public List<int> DamageList { get; private set; }
+        public bool IsMultiTarget {  get; private set; }
+        public int ManaCost {  get; private set; } // 마나 소모량
+        public string Content { get; private set; } // 스킬 설명
+    }
+}
