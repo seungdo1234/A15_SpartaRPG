b775f14addcf23f9cdca48212c0153fc40530d57
Devwons93 <jion933@gmail.com>
2024-05-01 10:49:36 +0900

    chore: PlayerClass 이름 변경 및 스킬 로직 변경
    PlayerClass > UnitType으로 Enum변경
    SkillDataManager 싱글톤 클래스 추가
    Skill 및 공격 로직 변경

--- Scripts/GameData/SkillData.cs
+++ Scripts/GameData/SkillData.cs
@@ -1,23 +1,36 @@
-﻿namespace TextRPG
+﻿using TextRPG.Scripts.Manager;
+
+namespace TextRPG
 {
     public class SkillData
-    {
-        private static int idCount = 0;
+    {  
         public int ID { get; private set; }
         public string Name { get; private set; }
         public int MaxTargetCount { get; private set; }
         public bool IsMultiTarget { get; private set; }
         public int ManaCost { get; private set; } // 마나 소모량
         public string Content { get; private set; } // 스킬 설명
+        public UnitType ClassType { get; private set; }
 
-        public SkillData(string name, int maxTarget, bool isMulti, int manaCost, string content)
+        public SkillData(int id)
         {
-            ID = idCount++;
-            Name = name;
-            MaxTargetCount = maxTarget;
-            IsMultiTarget = isMulti;
-            ManaCost = manaCost;
-            Content = content;
+            SkillData skill;
+            SkillDataManager.Instance.TryGetValue(id, out skill);
+        }
+
+        public virtual string CastSkill(Unit caster, Unit target)
+        {
+            string result;
+            string? critStr = caster.IsCriticalHit();
+            float critRate = critStr != null ? caster.CriticalDamage : 1f;             
+            float skillRate = IsMultiTarget ? 2 : 1.5f;            
+            int damage = caster.GetDamagePerHit();
+                                    
+            damage = Convert.ToInt32(Math.Round(damage * skillRate * critRate));
+            target.OnDamaged(damage);
+            result = $"[데미지 {damage}] " + critRate;
+
+            return result;
         }
     }
 }
---
+++ Scripts/GameData/Skills/CrisisEvasion.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPG
+{
+    public class CrisisEvasion : SkillData
+    {
+        public CrisisEvasion(int id) : base(id)
+        {
+        }
+
+        public override string CastSkill(Unit caster, Unit target)
+        {
+            string result;
+            string? critStr = caster.IsCriticalHit();
+            float critRate = critStr != null ? caster.CriticalDamage : 1f;
+            float skillRate = (caster.MaxHealth - caster.Health) > 0 ? (caster.MaxHealth - caster.Health) * 0.01f : 1f;
+            skillRate += 1.5f;
+            int damage = caster.GetDamagePerHit();
+                        
+            damage = Convert.ToInt32(Math.Round(damage * skillRate * critRate));
+            target.OnDamaged(damage);
+            result = $"[데미지 {damage}] " + critRate;
+
+            return result;
+        }
+    }
+}
