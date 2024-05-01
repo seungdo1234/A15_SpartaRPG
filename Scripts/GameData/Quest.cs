826b293657c270c4799f0920dae29ac9b600b668
Chloe <144107013+Charen523@users.noreply.github.com>
2024-05-01 17:07:32 +0900

    [feat] Quest 객체 생성

--- Scripts/GameData/Quest.cs
+++ Scripts/GameData/Quest.cs
@@ -8,9 +8,12 @@ namespace TextRPG.Scripts
 {
     public class Quest
     {
-        string QuestName { get; set; }
+        public string QuestName { get; set; }
+        public int CurrentProgress { get; set; }
+        public int TotalProgress {  get; set; }
+        public int? RewardGold { get; set; }
+        public Item? RewardItem { get; set; }
+        public string QuestContent { get; set; } 
 
-        //몬스터 처치, 처치한 몬스터 종류, 특정 스테이지 도달, 게임 클리어(보상?) 
-        string QuestType { get; set; } 
     }
 }
