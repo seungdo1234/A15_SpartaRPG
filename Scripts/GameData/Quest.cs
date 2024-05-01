92f29462ba2da90718a575036e52d522b734020d
Chloe <144107013+Charen523@users.noreply.github.com>
2024-05-01 14:49:23 +0900

    [feat] quest 만드는 중 저장

--- Scripts/GameData/Quest.cs
+++ Scripts/GameData/Quest.cs
@@ -8,7 +8,9 @@ namespace TextRPG.Scripts
 {
     public class Quest
     {
+        string QuestName { get; set; }
 
+        //몬스터 처치, 처치한 몬스터 종류, 특정 스테이지 도달, 게임 클리어(보상?) 
         string QuestType { get; set; } 
     }
 }
