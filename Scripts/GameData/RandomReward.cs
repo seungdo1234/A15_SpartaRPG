1a7c681f2e54b254f062b409052f31c9966e86bc
seungdo1234 <86179438+seungdo1234@users.noreply.github.com>
2024-05-03 20:42:49 +0900

    개발중..

---
+++ Scripts/GameData/RandomReward.cs
@@ -0,0 +1,17 @@
+﻿
+namespace TextRPG
+{
+    public struct Reward
+    {
+        public EquipItem rewardEquipItem; // 던전 보상 추가 예정
+        public ConsumableItem rewardConsumableItem; // 던전 보상 추가 예정
+        public int gold;
+    }
+    public class RandomReward
+    {
+        
+        Random random = new Random();
+
+        public void 
+    }
+}
