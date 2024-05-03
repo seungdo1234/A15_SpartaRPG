c1bd47b053b5c1036e35513ba606fd1d6ebcdf53
seungdo1234 <86179438+seungdo1234@users.noreply.github.com>
2024-05-03 16:20:23 +0900

    :art: 소비 아이템 사용 함수, 인벤토리 표시

--- Scripts/GameData/Items/ConsumableItem.cs
+++ Scripts/GameData/Items/ConsumableItem.cs
@@ -3,10 +3,10 @@ namespace TextRPG
 {
     public class ConsumableItem : Item
     {
-        public EConsumableType ConsumableType { get;private set; }
+        public EConsumableType ConsumableType { get; private set; }
         public int Value { get; private set; }
 
-        public ConsumableItem(string itemName, EItemRank itemRank, EConsumableType consumableType,int value ,string desc, int gold )
+        public ConsumableItem(string itemName, EItemRank itemRank, EConsumableType consumableType, int value, string desc, int gold)
         {
             ItemName = itemName;
             ItemRank = itemRank;
@@ -14,6 +14,20 @@ namespace TextRPG
             Value = value;
             Desc = desc;
             Gold = gold;
+        }
+
+        public void UseItem() // 포션 사용
+        {
+            switch (ConsumableType)
+            {
+                case EConsumableType.HEALTH:
+                    GameManager.instance.Player.RecoveryHealth(Value);
+                    break;
+                case EConsumableType.MANA:
+                    GameManager.instance.Player.RecoveryMana(Value);
+                    break;
+            }
+            GameManager.instance.Player.RemoveConsumableItem(ItemName);
         }
     }
 }
